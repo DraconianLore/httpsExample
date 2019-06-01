61aded64d3884e8be6210afad059f76d2f15aa57
Steven Wing <steven.wing82@gmail.com>
2019-06-01 18:31:11 +0000

    I can now display the entire content of the page(which cuts short so cannot possibly be the entire script)

--- https_example.js
+++ https_example.js
@@ -2,8 +2,8 @@ var https = require('https');
 
 
 var options = {
-    host: 'www.example.org',
-    path: '/'
+    host: 'stream-large-file.herokuapp.com',
+    path: '/give-me-stuff-now'
 };
 
 // calledd by https when the request is made
